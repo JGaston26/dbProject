2b4da645ac84b0db8d9579038ef3ba5741feacd4
DashielL23 <144473374+DashielL23@users.noreply.github.com>
2025-04-01 12:29:44 -0400

    Add files via upload

--- CourseOffering.java
+++ CourseOffering.java
@@ -1,16 +1,18 @@
 import java.util.ArrayList;
 public class CourseOffering {
+    private boolean isAPClass;
     private int timesAvailable;
     private ArrayList<Integer> periodsInUse = new ArrayList<>();
     private ArrayList<String> roomsInUse = new ArrayList<>();
     private int courseId;
-    private int teacherId;
+    private Teacher teacher;
     private int id;
-    public CourseOffering(int courseId, int teacherId, int id){
+    public CourseOffering(int courseId, Teacher teacher, int id, boolean isAP){
         timesAvailable= (int)(Math.random()*6);
         this.courseId=courseId;
-        this.teacherId=teacherId;
+        this.teacher=teacher;
         this.id=id;
+        isAPClass = isAP;
     }
 
     public void addPeriodInuse(int x){
--- Main.java
+++ Main.java
@@ -4,17 +4,22 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+    static ArrayList<Course> apCourses = new ArrayList<>();
     public static void main(String[] args) {
         ArrayList<String> studentNames = getFileData("Students");
         ArrayList<String> teacherNames = getFileData("TeachersAndDepartments"); //no dupes
         ArrayList<String> roomNames = getFileData("Rooms");
+        ArrayList<String> courseNames = getFileData("Classes");
 
-       // ArrayList<Teacher> teacherData = createTeachers(teacherNames);
+        ArrayList<Course> courseData = createCourses(courseNames);
+        ArrayList<Teacher> teacherData = createTeachers(teacherNames);
         ArrayList<Student> studentData = createStudents(studentNames);
         ArrayList<Room> roomData = createRooms(roomNames);
-       // System.out.println(teacherData);
-        System.out.println(studentData);
-        System.out.println(roomData);
+
+        // System.out.println(teacherData);
+        // System.out.println(studentData);
+        // System.out.println(roomData);
+        System.out.println(courseData);
     }
 
     public static ArrayList<Teacher> createTeachers(ArrayList<String> teacherNames){
@@ -39,14 +44,13 @@ public class Main {
     }
     public static ArrayList<Student> createStudents(ArrayList<String> studentNames) {
         ArrayList<Student> returnList = new ArrayList<>();
-        int deptId=1;
         int studentId=1;
         for(String name: studentNames){
             returnList.add(new Student(studentId));
             studentId++;
         }
-        for(int i=1; i< returnList.size(); i++){
-            System.out.println(returnList.get(i).getId());
+        for(int i=0; i< returnList.size(); i++){
+           // System.out.println(returnList.get(i).getId());
         }
         return returnList;
     }
@@ -62,6 +66,41 @@ public class Main {
                     "("+ i + ", '" + returnList.get(i).getName() + "');");
         }
         return returnList;
+    }
+    public static ArrayList<Course> createCourses(ArrayList<String> courses){
+        ArrayList<Course> returnList = new ArrayList<>();
+        int courseID=1;
+        for(String name: courses){
+            int typeId=1;
+            if(name.contains("Common Core") || name.contains("Regents")) typeId= 2;
+            else if(name.contains("AP ")){
+                typeId=3;
+                apCourses.add(new Course(name, courseID,typeId));
+            }
+            returnList.add(new Course(name, courseID,typeId));
+            courseID++;
+        }
+
+        return returnList;
+    }
+    public static ArrayList<CourseOffering> createCourseOffering(ArrayList<Course> courses, ArrayList<Teacher> teachers, ArrayList<Room> rooms){
+        /*
+            When assigning teachers to courses its important to note:
+            Teachers cannot teach 2 classes at the same time, so make sure to check they are available in the period we're checking for
+            also for AP classes the teacher needs to be available for the double, so make sure thats also good to go.
+            We can use random courses
+
+            Step 1: Select random teacher and course to create a course offering
+            Step 2: Using the times available, properly remove periods available from the room/teacher objects and make sure to account for APs
+            Step 3: Run until no more teacher periods / rooms to use etc etc
+         */
+        ArrayList<CourseOffering> returnList = new ArrayList<>();
+        ArrayList<CourseOffering>
+        int courseOfferingId=1;
+        for(int i = 0; i < 500; i++){
+            returnList.add(CourseOffering(courseOfferingId,))
+        }
+        return null;
     }
     public static ArrayList<String> getFileData(String fileName) {
         ArrayList<String> fileData = new ArrayList<String>();
